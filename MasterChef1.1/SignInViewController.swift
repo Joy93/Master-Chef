023d3c549d82051095537a093ea25f925e6a8450
YEJING ZHOU <zhouyj0118@gmail.com>
2017-03-05 19:56:01 -0600

    first commit

---
+++ MasterChef1.1/SignInViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SignInViewController.swift
+//  MasterChef1.1
+//
+//  Created by yejing zhou on 2017/2/14.
+//  Copyright © 2017年 yejing zhou. All rights reserved.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
